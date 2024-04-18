ab9e59b539a55a98395b0713c7a26606eddd2b95
rinorocks8 <rinorocks@gmail.com>
2024-04-18 17:48:01 -0400

    test

--- api/index.ts
+++ api/index.ts
@@ -3,4 +3,3 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   return res.status(200).send({});
 }
-// deployment test1
